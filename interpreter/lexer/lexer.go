7136246bb17d68769859316a1b0b309f84ca05e5
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2024-08-02 19:18:17 +0100

    have interpreter successfully support identifiers, keywords, whitespaces and ints

--- interpreter/lexer/lexer.go
+++ interpreter/lexer/lexer.go
@@ -7,10 +7,10 @@ import (
 // remember that a struct, if no value is passed is initialised
 // with the zero value
 type Lexer struct {
-	input string
-	position int // current position in input (points to current char)
-	readPosition int // current reading position in input (after next char)
-	ch byte // current chat under examination
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after next char)
+	ch           byte // current chat under examination
 }
 
 func New(input string) *Lexer {
@@ -18,7 +18,7 @@ func New(input string) *Lexer {
 	l.readChar()
 	return l
 }
-func (l *Lexer) readChar(){
+func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
@@ -27,34 +27,75 @@ func (l *Lexer) readChar(){
 	l.position = l.readPosition
 	l.readPosition += 1
 }
-func (l *Lexer) NextToken() token.Token  {
+func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+	l.eatWhitespace()
 	switch l.ch {
-    case '=':
-        tok = newToken(token.ASSIGN, l.ch)
-    case ';':
-        tok = newToken(token.SEMICOLON, l.ch)
-    case '(':
-        tok = newToken(token.LPAREN, l.ch)
-    case ')':
-        tok = newToken(token.RPAREN, l.ch)
-    case ',':
-        tok = newToken(token.COMMA, l.ch)
-		case '+':
-			tok = newToken(token.PLUS, l.ch)
-		case '{':
-			tok = newToken(token.LBRACE, l.ch)
-		case '}':
-			tok = newToken(token.RBRACE, l.ch)
-		case 0:
-			tok.Literal = ""
-			tok.Type = token.EOF
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+		// basically lexer needs to identify if it is
+		// a letter and keep reading until it encounterss a non-letter-char
+		// we then determine if it is an identifier or keyword
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
 		}
-	
-		l.readChar()
-		return tok
-	
+	}
+
+	l.readChar()
+	return tok
+
 }
-func newToken(tokenType token.TokenType, ch byte) token.Token  {
+func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+func (l *Lexer) eatWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= 9
 }
